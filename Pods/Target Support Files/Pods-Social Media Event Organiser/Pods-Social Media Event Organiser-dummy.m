d2c50ac07a9858e64d832c499b8d453533c197bb
seenashafai <seenas@btinternet.com>
2018-04-18 15:43:57 +0100

    Integrate Firebase, create xcworkspace

---
+++ Pods/Target Support Files/Pods-Social Media Event Organiser/Pods-Social Media Event Organiser-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Social_Media_Event_Organiser : NSObject
+@end
+@implementation PodsDummy_Pods_Social_Media_Event_Organiser
+@end
---
+++ Pods/Target Support Files/Pods-Social Media Event OrganiserTests/Pods-Social Media Event OrganiserTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Social_Media_Event_OrganiserTests : NSObject
+@end
+@implementation PodsDummy_Pods_Social_Media_Event_OrganiserTests
+@end
